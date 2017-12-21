7710a9f6f7b4128d1733ef908f866921eb8189bc
github username <andrewcohn0@gmail.com>
2017-12-21 14:55:05 -0500

    update to rails 5 && change to render json

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,13 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    render json: @posts
   end
 
   def show
     @post = Post.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}]) }
-    end
+    render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}])
   end
 
   def new
