454bba9e202b0275959ff3fabe62efdc6ddf4a67
Zachary Lehmann <zlehmann@gmail.com>
2019-06-04 22:33:12 +0000

    Automatically backed up by Learn

--- spec/models/author_model_spec.rb
+++ spec/models/author_model_spec.rb
@@ -1,11 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "author_model" do
-  before do
-    test_var = "yes"
-  end
-
   it "lets me pass my Learn tests" do
+    test_var = "yes"
     expect(test_var).to eq("yes")
   end
 end
